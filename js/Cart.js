80b1e49a69896e19b3c56f277d835a062871251f
Cristian Agüero <106722952+Aguerocristianm@users.noreply.github.com>
2024-02-28 07:33:35 -0300

    Add files via upload

--- js/Cart.js
+++ js/Cart.js
@@ -1,17 +1,18 @@
 class Cart {
     constructor(list = []) {
         this.cart = list;
+        this.loadCartFromLocalStorage();
     }
 
-    addToCart({ id, name, price }) {
-        const index = this.cart.findIndex(product => product.id == id);
-        if (index == -1) {
-            this.cart.push({ id, name, price, units: 1 });
+    addToCart({ id_product, name, price }) {
+        const index = this.cart.findIndex(product => product.id_product === id_product);
+        if (index === -1) {
+            this.cart.push({ id_product, name, price, units: 1 });
         } else {
             this.cart[index].units += 1;
         }
 
-        localStorage.setItem('cart', JSON.stringify(this.cart));
+        this.saveCartToLocalStorage();
     }
 
     getProducts() {
@@ -19,11 +20,26 @@ class Cart {
     }
 
     getCount() {
-        const count = this.cart.reduce((cant, product) => { return cant + product.units }, 0)
-        return count;
+        return this.cart.reduce((count, product) => count + product.units, 0);
     }
 
     getSum() {
-        return this.cart.reduce((acum, product) => { return acum + (product.units * product.price) }, 0)
+        return this.cart.reduce((total, product) => total + (product.units * product.price), 0);
+    }
+
+    saveCartToLocalStorage() {
+        localStorage.setItem('cart', JSON.stringify(this.cart));
+    }
+
+    loadCartFromLocalStorage() {
+        const savedCart = localStorage.getItem('cart');
+        if (savedCart) {
+            this.cart = JSON.parse(savedCart);
+        }
+    }
+    clearCart() {
+        this.cart = [];
+        this.saveCartToLocalStorage();
+        renderCart()
     }
 }
--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
-const modal = new bootstrap.Modal('#modalCarrito', {});
+const modalElement = document.querySelector('#modalCarrito');
+const modal = new bootstrap.Modal(modalElement, {});
+const btnUmbrella = document.querySelector('#btnUmbrella')
 const btnModalCarrito = document.querySelector('#btnModalCarrito');
 const cartCount = document.querySelector('#cartCount');
 const cartSum = document.querySelector('#cartSum');
@@ -9,86 +11,81 @@ const modalListProducts = document.querySelector('#modalListProducts');
 const btnClose = document.querySelector('#btnClose');
 const btnSave = document.querySelector('#btnSave');
 const btnOrder = document.querySelector('#btnOrder');
-const btnOrder2 = document.querySelector('#btnOrder2')
-const btnAddToCart = document.querySelector('#addToCart')
-const btnUmbrella = document.querySelector("#btnUmbrella");
+const btnOrder2 = document.querySelector('#btnOrder2');
 const listCart = JSON.parse(localStorage.getItem('cart')) || [];
 const cart = new Cart(listCart);
+const btnClear = document.querySelector('#btnClear');
 
-cartCount.innerText = cart.getCount();
+if (btnUmbrella) {
+    btnUmbrella.addEventListener('click', () => {
+        swal("¡Bien!", "Gracias por hacer click aqui ;)", "success");
+        modal.hide();
+    })
+}
 
+if (btnModalCarrito) {
+    btnModalCarrito.addEventListener('click', () => {
+        const list = cart.getProducts();
+        cartSum.innerText = cart.getSum();
+        renderCart(list);
+        modal.show();
+    });
+}
 
-btnModalCarrito.addEventListener('click', function () {
-    const list = cart.getProducts();
-    cartSum.innerText = cart.getSum();
-
-    redenCart(list);
-
-    modal.show();
-})
-
-btnClose.addEventListener('click', () => {
-    modal.hide();
-})
-
+if (btnClose) {
+    btnClose.addEventListener('click', () => {
+        modal.hide();
+    });
+}
 inputSearch.addEventListener('input', (event) => {
     const search = event.target.value;
     const newList = products.filter((product) => product.name.toLowerCase().includes(search.toLowerCase()));
     renderProducts(newList);
 })
 
-btnOrder.addEventListener('click', () => {
-    products.sort((a, b) => {
-        if (a.price < b.price) {
-            return -1
-        }
-        if (a.price > b.price) {
-            return 1
-        }
-
-        return 0
-    })
-
-    renderProducts(products)
-})
-btnOrder2.addEventListener('click', () => {
-    products.sort((b, a) => {
-        if (a.price < b.price) {
-            return -1
-        }
-        if (a.price > b.price) {
-            return 1
-        }
-
-        return 0
-    })
-
-    renderProducts(products)
-})
-btnSave.addEventListener('click', () => {
-    swal("¡Bien!", "Gracias por tu compra :)", "success");
-    modal.hide()
-})
-selectCategory.addEventListener('change', (e) => {
-    const id_category = selectCategory.value;
-    console.log('Cateoría', id_category )
-
-    filtroCategoria( id_category )
-})
-const filtroCategoria = ( id_category ) => {
-    const newList = products_list.filter (  (product) => product.id_category == id_category );
-    renderProducts(newList);
+if (btnOrder) {
+    btnOrder.addEventListener('click', () => {
+        products.sort((a, b) => a.price - b.price);
+        renderProducts(products);
+    });
 }
+
+if (btnOrder2) {
+    btnOrder2.addEventListener('click', () => {
+        products.sort((a, b) => b.price - a.price);
+        renderProducts(products);
+    });
+}
+
+if (btnSave) {
+    btnSave.addEventListener('click', () => {
+        swal("¡Bien!", "Gracias por tu compra :)", "success");
+        modal.hide();
+    });
+}
+
+if (selectCategory) {
+    selectCategory.addEventListener('change', (e) => {
+        const id_category = selectCategory.value;
+        filterCategory(id_category);
+    });
+}
+
+const filterCategory = (id_category) => {
+    const newList = products.filter((product) => product.id_category == id_category);
+    renderProducts(newList);
+};
+
 const renderProducts = (list) => {
     listProducts.innerHTML = '';
     list.forEach(product => {
-        listProducts.innerHTML += // html
+        listProducts.innerHTML +=
             `<div class="col-sm-4 col-md-3">
                 <div class="card p-3">
                     <h4>${product.name}</h4>
                     <img class="img-fluid" src="${product.img}" alt="${product.name}">
                     <h3 class="text-center">$${product.price} </h3>
-                    <button id="${product.id_product} " type="button" class="btn btnAddCart">
+                    <button id="${product.id_product}" type="button" class="btn btnAddCart">
                         <i class="fa-solid fa-cart-plus"></i> Agregar
                     </button>
                 </div>
@@ -98,15 +95,7 @@ const renderProducts = (list) => {
     const btns = document.querySelectorAll('.btnAddCart');
     btns.forEach(btn => {
         btn.addEventListener('click', addToCart);
-        Toastify({
-
-            text: "This is a toast",
-
-            duration: 3000
-
-        }).showToast();
     });
-
 }
 
 const addToCart = (e) => {
@@ -114,40 +103,40 @@ const addToCart = (e) => {
     const product = products.find(item => item.id_product == id);
     cart.addToCart(product);
     cartCount.innerText = cart.getCount();
+    console.log(product)
 
     Toastify({
         close: true,
-        text:"Producto agregado al carro ;)",
+        text: "Producto agregado al carro",
         gravity: "bottom",
-        duration: 3000,
+        duration: 2500,
         style: {
-            background:"#f5ebe0",
+            background: "#d5bdaf",
             color: "white"
         },
     }).showToast();
 }
 
-const redenCart = (list) => {
+const renderCart = (list) => {
     modalListProducts.innerHTML = '';
     list.forEach(product => {
-        modalListProducts.innerHTML += // html
+        modalListProducts.innerHTML +=
             `<tr>
-                <td> ${product.name} </td>
-                <td> ${product.units}</td>
+                <td>${product.name}</td>
+                <td>${product.units}</td>
                 <td>$${product.price}</td>
                 <td>$${product.price * product.units}</td>
-
             </tr>`
     });
 }
-const renderCategory = ( list) => {
+
+const renderCategory = (list) => {
     selectCategory.innerHTML = '';
-    list.forEach( category => {
-        selectCategory.innerHTML += // html
-        `<option value="${category.id_category}"> ${category.name}</option>`
+    list.forEach(category => {
+        selectCategory.innerHTML +=
+            `<option value="${category.id_category}"> ${category.name}</option>`
     });
 }
-
 
 const getProducts = async () => {
 
@@ -156,19 +145,23 @@ const getProducts = async () => {
         const resp = await fetch(endPoint);
         const json = await resp.json();
 
-        const { products, category } = json;
-        products_list = products;
-        renderProducts( products);
-        renderCategory( category)
+        const { products: fetchedProducts, category } = json;
+        products = fetchedProducts;
+        renderProducts(products);
+        renderCategory(category);
 
     } catch (error) {
         Swal.fire({
             title: "Error",
-            text: 'Ocurrio un error al mostrar los Productos, por favor intente más tarde :)',
+            text: 'Ocurrió un error al mostrar los Productos, por favor inténtelo más tarde',
             icon: "error",
             confirmButtonText: 'Aceptar'
         });
+        console.log(error);
     }
 }
-
+btnClear.addEventListener('click', () => {
+    cart.clearCart();
+    renderCart()
+});
 getProducts();
